945a2446e2ebd5bbbda0ff3dd2d63e5023ddbbf0
AZKvgkv <AZK19991219@outlook.com>
2025-08-04 10:52:39 +0800

    feat: 添加按位运算功能和printBinary函数
    添加了一个新的函数`printBinary`，用于打印8位二进制数的每一位。
    在`main`函数中，添加了按位运算的示例，包括左移、右移、按位与、按位或、按位异或以及按位取反操作。这些操作展示了如何使用按位运算符来实现位移和交换两个整数的值。
    左移操作符`<<`和右移操作符`>>`用于移位操作，例如左移2位相当于乘以4。
    按位与操作符`&`、按位或操作符`|`和按位异或操作符`^`用于对每一位执行逻辑运算。
    按位取反操作符`~`用于对每一位执行取反操作。
    在按位取反操作后，使用`printBinary`函数打印取反前后的二进制数。

--- operator.c
+++ operator.c
@@ -4,6 +4,10 @@
 
 typedef uint32_t u32;
 typedef int32_t i32;
+typedef uint8_t u8;
+
+void printBinary(u8 num);
+
 int main() {
   u32 apple = 5;
   u32 orange = 10;
@@ -21,5 +25,40 @@ int main() {
   printf("Res: %" PRIi32 "\n", res);
   printf("After Number: %" PRIi32 "\n", number);
 
+  // 按位运算
+
+  // 左移n位相当于乘以2的n次方
+  // 例如：左移2位相当于乘以4
+  // 左移操作符是 <<，右移操作符是 >>
+  // 按位与操作符是 &，按位或操作符是 |，按位异或操作符是 ^
+  u8 num = 22; // 22 = 10110
+  num <<= 2;   // 左移2位，变为 1011000
+  printf("Left Shift: %" PRIu8 "\n", num);
+  num >>= 3;
+  printf("Right Shift: %" PRIu8 "\n", num);
+
+  // 使用按位运算来实现变量的交换
+  u32 a = 10, b = 20; // 10 = 1010; 20 = 10100
+  printf("Before Swap: a = %" PRIu32 ", b = %" PRIu32 "\n", a, b);
+  a ^= b;
+  b ^= a;
+  a ^= b;
+  printf("After Swap: a = %" PRIu32 ", b = %" PRIu32 "\n", a, b);
+
+  // 按位取反
+  u8 status = 0b10101010; // 二进制表示
+  printf("Original Status: ");
+  printBinary(status);
+  printf("\n");
+  status = ~status; // 按位取反
+  printf("After Inversion: ");
+  printBinary(status);
+  printf("\n");
   return 0;
+}
+
+void printBinary(u8 num) {
+  for (int i = 7; i >= 0; i--) {
+    printf("%d", (num >> i) & 1);
+  }
 }
