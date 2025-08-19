6b0a54e15e88c32c4bd5c79676806eb12f4f6b9b
AZKvgkv <AZK19991219@outlook.com>
2025-08-19 13:58:08 +0800

    feat(core): 添加猜数游戏功能
    在代码中添加了一个生成随机数的猜数游戏案例。游戏会生成一个1到100之间的随机数，然后让用户尝试猜测这个数字。用户可以通过输入1到100之间的整数来进行猜测，如果输入错误或超出范围，程序会提示重新输入。如果用户输入0，游戏会结束。如果用户猜的数字小了，程序会提示猜小了；如果猜的数字大了，程序会提示猜大了；如果猜对了，程序会恭喜用户并结束游戏。

--- loop_do.c
+++ loop_do.c
@@ -1,16 +1,45 @@
 #include "inttypes.h"
 #include "stdint.h"
 #include "stdio.h"
+#include <stdlib.h>
+#include <time.h>
 
 #define TOTAL_LAPS 6
 typedef uint32_t u32;
 
 int main() {
   printf("do_while loop\n");
-  u32 current_lap = 0;
+  /* 跑圈案例 */
+  // u32 current_lap = 0;
+  // do {
+  //   current_lap++;
+  //   printf("lap %" PRIu32 "\n", current_lap);
+  // } while (current_lap < TOTAL_LAPS);
+
+  // 生成随机数的猜数游戏案例
+  u32 secret_number, guess;
+  srand(time(NULL));
+  // 生成1到100之间的随机数
+  secret_number = rand() % 100 + 1;
+
   do {
-    current_lap++;
-    printf("lap %" PRIu32 "\n", current_lap);
-  } while (current_lap < TOTAL_LAPS);
+    puts("请输入1到100之间的整数：");
+    if (scanf_s("%" PRIu32, &guess) != 1 || guess < 0 || guess > 100) {
+      puts("输入错误，请输入1到100之间的整数！");
+      continue; // 如果输入错误，重新开始循环
+    }
+
+    if (guess == 0) {
+      puts("游戏结束！");
+      break; // 如果输入0，结束游戏
+    } else if (guess < secret_number) {
+      puts("猜小了，请再试一次！");
+    } else if (guess > secret_number) {
+      puts("猜大了，请再试一次！");
+    } else {
+      puts("恭喜你猜对了！");
+    }
+  } while (guess != secret_number);
+
   return 0;
 }
